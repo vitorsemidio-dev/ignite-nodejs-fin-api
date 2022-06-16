3ce569a2e78fdfdf016955d6aaca62d3ad10d5d8
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-16 03:53:45 -0300

    refactor: verifição existência conta usando middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const customers = [];
 
 app.use(express.json());
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+  if (!customer) {
+    return res.status(404).json({ error: "Customer not found" });
+  }
+
+  req.customer = customer;
+  next();
+}
+
 app.post("/account", (req, res) => {
   const { cpf, name } = req.body;
   const id = uuidv4();
@@ -29,14 +41,8 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
-app.get("/statement/", (req, res) => {
-  const { cpf } = req.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return res.status(404).json({ error: "Customer not found" });
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
 
   return res.json(customer.statement);
 });
