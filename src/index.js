51874558b87f3f0cc7a63aec5018c15254272cff
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-16 03:45:52 -0300

    feat: requisito e rn extrato cliente

--- src/index.js
+++ src/index.js
@@ -29,6 +29,18 @@ app.post("/account", (req, res) => {
   return res.status(201).send();
 });
 
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(404).json({ error: "Customer not found" });
+  }
+
+  return res.json(customer.statement);
+});
+
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
 });
