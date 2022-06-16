8c11ed0e503b4a41569eda273e59770a4a82f709
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-06-16 03:36:22 -0300

    feat: requisito criar conta

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+const customers = [];
+
+app.use(express.json());
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
